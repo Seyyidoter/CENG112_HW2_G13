5be65a80b002eb0c5b903539232a064ee096f59b
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-06-02 18:04:39 +0300

    Update GenericHistory.java

--- src/model/GenericHistory.java
+++ src/model/GenericHistory.java
@@ -1,6 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenericHistory<T> {
-    
+    private List<T> historyList;
+
+    public GenericHistory() {
+        historyList = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        historyList.add(item);
+    }
+
+    public List<T> getAll() {
+        return historyList;
+    }
+
+    public void display() {
+        int count = 1;
+        for (T item : historyList) {
+            System.out.println(count++ + ". " + item.toString());
+        }
+    }
 }
+
