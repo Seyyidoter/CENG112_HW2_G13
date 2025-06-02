9da63dd7eb92834bcea3047b292b0da8eeafe769
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-06-02 18:05:39 +0300

    Update GenericPriorityQueue.java

--- src/model/GenericPriorityQueue.java
+++ src/model/GenericPriorityQueue.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericPriorityQueue<T extends Comparable<T>> {
+
     private GenericQueue<T> highPriorityQueue;
     private GenericQueue<T> mediumPriorityQueue;
     private GenericQueue<T> lowPriorityQueue;
@@ -11,5 +15,42 @@ public class GenericPriorityQueue<T extends Comparable<T>> {
         lowPriorityQueue = new GenericQueue<>();
     }
 
-}
+    public void offer(T item) {
+        int priorityValue = extractPriorityValue(item);
+        switch (priorityValue) {
+            case 3:
+                highPriorityQueue.enqueue(item);
+                break;
+            case 2:
+                mediumPriorityQueue.enqueue(item);
+                break;
+            case 1:
+                lowPriorityQueue.enqueue(item);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown priority for item: " + item.toString());
+        }
+    }
+
+    public T poll() {
+        if (!highPriorityQueue.isEmpty()) {
+            return highPriorityQueue.dequeue();
+        } else if (!mediumPriorityQueue.isEmpty()) {
+            return mediumPriorityQueue.dequeue();
+        } else if (!lowPriorityQueue.isEmpty()) {
+            return lowPriorityQueue.dequeue();
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return highPriorityQueue.isEmpty() && mediumPriorityQueue.isEmpty() && lowPriorityQueue.isEmpty();
+    }
+
+    public List<T> getAll() {
+        List<T> all = new ArrayList<>();
+        all.addAll(highPriorityQueue.getAll());
+        all.addAll(mediumPriorityQueue.getAll());
+        all.addAll(lowPr
+
 
