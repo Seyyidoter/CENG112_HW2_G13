3f7932e54cda7d39a4506cc1b4417ae1eed404d9
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-06-03 14:49:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+//import necessary files
+import command.Command;
+import command.CommandProcessor;
+import util.FileIO;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello, World!");
-        
+
+        // file path
+        String filePath = "src/example_commands.csv";
+
+        // read commands
+        List<Command> commands = FileIO.readCommandsFromCSV(filePath);
+
+        // run them
+        CommandProcessor processor = new CommandProcessor();
+        processor.process(commands);
     }
 }
